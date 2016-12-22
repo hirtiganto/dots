2be646e78a49799682202095adfab06599f8e1a2
Jakub Pustelnik <jhirtiganto@gmail.com>
2016-12-22 22:47:59 +0100

    Canvas now fill whole window, added code that calls dot functions

--- sketch.js
+++ sketch.js
@@ -1,15 +1,17 @@
 var dots = [];
 
 function setup() {
-  createCanvas(800,500);
-  for (var i = 0; i <= 100; i++){
+  createCanvas(windowWidth, windowHeight);
+  for (var i = 0; i <= 250; i++){
     dots.push(new Dot())
   }
 }
 
 function draw() {
   background(0);
-  for (var i = 0; i <= dots.length; i++){
-    dots[i].render();
+  for (var i = 0; i < dots.length; i++){
+    dots[i].render_dot();
+    dots[i].move_dot();
+    dots[i].check_edge();
   }
 }
