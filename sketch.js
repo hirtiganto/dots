0e2bea27f25e331ba2e28e549ba2d3933f83cf66
Jakub Pustelnik <jhirtiganto@gmail.com>
2016-12-22 18:03:14 +0100

    Added some code that initializes some dots and displays them on the canvas

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
+var dots = [];
+
 function setup() {
   createCanvas(800,500);
+  for (var i = 0; i <= 100; i++){
+    dots.push(new Dot())
+  }
 }
 
 function draw() {
   background(0);
+  for (var i = 0; i <= dots.length; i++){
+    dots[i].render();
+  }
 }
