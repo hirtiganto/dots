78cf9f39b916a7d9ffe2022b79f9406bca0a157d
Jakub Pustelnik <jhirtiganto@gmail.com>
2016-12-22 22:47:01 +0100

    Modified dot.js: removed acceleration, dots are now moved only by velocity, added code that checks if dot hit the edge of the screen

--- dot.js
+++ dot.js
@@ -1,5 +1,7 @@
-function Dot(){
+function Dot() {
   this.position = createVector(random(0,width),random(0,height));
+  this.velocity = createVector(random(0.5, 2.5),0);
+
   this.radius = 25;
   this.color = {
     r: random(0,255),
@@ -8,9 +10,25 @@ function Dot(){
     a: 100,
   };
 
-  this.render = function(){
+  this.render_dot = function() {
     noStroke();
     fill(this.color.r,this.color.g,this.color.b,this.color.a);
     ellipse(this.position.x,this.position.y,this.radius,this.radius);
+  }
+
+  this.check_edge = function() {
+    if (this.position.x >= windowWidth){
+      this.position.x = 0;
+    } else if (this.position.x <= 0) {
+      this.position.x = windowWidth;
+    } else if (this.position.y >= windowHeight) {
+      this.position.y = 0;
+    } else if (this.position.y <= 0) {
+      this.position.y = windowHeight;
+    }
+  }
+
+  this.move_dot = function() {
+    this.position.add(this.velocity);
   }
 }
