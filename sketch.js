3a978e39a47c677d5d88a76d512ffe84c2721c11
Jakub Pustelnik <jhirtiganto@gmail.com>
2016-12-23 17:31:50 +0100

    Added code that calls new dot functions

--- sketch.js
+++ sketch.js
@@ -2,15 +2,18 @@ var dots = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  for (var i = 0; i <= 250; i++){
+  for (var i = 0; i < 250; i++){
     dots.push(new Dot())
   }
 }
+
 
 function draw() {
   background(0);
   for (var i = 0; i < dots.length; i++){
     dots[i].render_dot();
+    dots[i].apply_wind();
+    dots[i].avoid_mouse();
     dots[i].move_dot();
     dots[i].check_edge();
   }
